08e1570b3b2c77e970f434a9cc19a7e525c3fb7e
Pete York <pete.york@softwire.com>
2015-10-02 15:28:14 +0100

    Started work on the solver by adding Head, Solution and Direction classes

---
+++ solver/Head.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallengeOne.Solver
+{
+    class Head
+    {
+        List<int> sequence;
+        List<Solution> solutions;
+        Direction direction = Direction.Right;
+        int index = 0;
+
+        public 
+
+    }
+}
