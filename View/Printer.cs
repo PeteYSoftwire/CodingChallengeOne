2e9639a3ae78120c9b87b5dc8b3f8b2381c62074
Pete York <pete.york@softwire.com>
2015-10-02 16:56:30 +0100

    added a printer show the partial solutions

---
+++ View/Printer.cs
@@ -0,0 +1,47 @@
+﻿using CodingChallengeOne.Solver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallengeOne.View
+{
+    class Printer
+    {
+        List<Solution> solutions;
+
+        public Printer(List<Solution> solutions)
+        {
+            this.solutions = solutions;
+        }
+
+        public void print()
+        {
+            solutions.ForEach((item)=>
+                {
+                    printPartialSolution(item);
+                });
+        }
+
+        private void printPartialSolution(Solution item)
+        {
+            int lastValueChangedIndex = item.getLastValueChangedIndex();
+            List<int> values = item.getValues();
+            String output = "";
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (i == lastValueChangedIndex)
+                {
+                    output = output + "*" + values[i].ToString() + "*";
+                }
+                else
+                {
+                    output = output + values[i].ToString();
+                }
+
+            }
+            System.Console.WriteLine(output);
+        }
+    }
+}
